48a4c1cf69750e5322cb03266b2d01dce2ac0d71
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 11:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var isDate = function (input) {
   //   write your code here
 	if (input instanceof Date && !isNaN(input)) return true
 	if (typeof input === 'string' && !isNaN(Date.parse(input))) return true
-	return false
+	else return false
 };
 
 // Do not change the code below.
